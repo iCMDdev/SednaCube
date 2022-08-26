2ff1b1481a7eabe479d8df3e2fef6feab902d4fa
CMD <80255379+iCMDgithub@users.noreply.github.com>
2022-08-26 14:32:01 +0300

    Create EEPROM-RESET.ino

---
+++ Code/EEPROM-RESET.ino
@@ -0,0 +1,29 @@
+// EEPROM RESET
+// This program "factory" resets the SednaCube by erasing and rewriting the EEPROM.
+
+// This program may be used to set a new switch state for the EJECT switch
+// and to remove previously saved logging data.
+
+#include <EEPROM.h>
+
+uint16_t a = 0; // address that is erased; incremented each time an address has ben erasedin loop()
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Starting EEPROM erase program.");
+}
+
+void loop()
+{
+  EEPROM.write(a, 255);
+  a = a + 1;
+  if (a == 512) {
+    float f = 0f;
+    uint8_t cnt = 0;
+    Serial.println("EEPROM ERASED SUCCESSFULLY");
+    EEPROM.put(200, f); // maximum altitude init
+    EEPROM.put(250, cnt); // maximum altitude count init
+    Serial.println("Done! Device's EEPROM was fully reset.");
+  }
+}
